771923619624ee638cbe30f24143509d95572584
bassil-97 <61696257+bassil-97@users.noreply.github.com>
2022-02-21 07:47:38 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
+import Dice from './images/icon-dice.svg';
+
+
 function App() {
+
+  const [newAdvice, setAdvice] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchNewAdvice = () => {
+    setIsLoading(true)
+    axios.get('https://api.adviceslip.com/advice')
+    .then(res => {
+      setAdvice({
+        advice: res.data['slip']['advice'],
+        id: res.data['slip']['id'],
+      });
+
+      setIsLoading(false);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='advice__container'>
+        <div className='advice_body'>
+          {newAdvice && <small>advice #{newAdvice.id}</small>}
+          {newAdvice && !isLoading && <h4>
+          “
+            {newAdvice.advice}
+          ”
+          </h4>}
+          {!newAdvice && !isLoading && <h4>click below to get a new advice</h4>}
+          {isLoading && <div className="loader"></div>}
+        </div>
+        <div className='divider'></div>
+        <div className='generate_advice_btn' onClick={fetchNewAdvice}>
+          <img src={Dice} alt="img-dice" />
+        </div>
+      </div>
     </div>
   );
 }
